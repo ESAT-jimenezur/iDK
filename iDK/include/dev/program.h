71e4fb8a959cf7daef35d998a722b8ac9e4e153e
Jose Luis Jimenez <ijos@ijos.es>
2015-05-17 22:12:27 +0200

    Program class added (implemented)

--- iDK/include/dev/program.h
+++ iDK/include/dev/program.h
@@ -1,49 +1,53 @@
 #ifndef __PROGRAM_H__
 #define __PROGRAM_H__
 
+#include "EDK3\dev\program.h"
+#include "EDK3\dev\gpumanager.h"
+#include "EDK3\dev\opengl.h"
 
 namespace iDK{
   namespace dev{
 
-    //class Shader;
-    //class Uniform;
+    class Shader;
+    class Uniform;
 
-    class Program {
+    class Program : public EDK3::dev::Program {
 
       public:
-        //virtual void attach(Shader *) = 0;
-        //virtual bool link(scoped_array<char> *link_log = 0L) = 0;
+        virtual void attach(EDK3::dev::Shader *shader) = 0;
+        virtual bool link(EDK3::scoped_array<char> *link_log = 0L) = 0;
         virtual void use() const = 0;
-        virtual  int get_attrib_location(const char *name) const = 0;
+        virtual int get_attrib_location(const char *name) const = 0;
         virtual int  get_uniform_position(const char *name) const = 0;
         virtual void set_uniform_value(
           const int uniform_pos,
-          const Type uniform_type,
+          const EDK3::Type uniform_type,
           const float *) const = 0;
         virtual void set_uniform_value(
           const int uniform_pos,
-          const Type uniform_type,
+          const EDK3::Type uniform_type,
           const int*) const = 0;
         virtual void set_uniform_value(
           const int uniform_pos,
-          const Type uniform_type,
+          const EDK3::Type uniform_type,
           const unsigned int*) const = 0;
 
-        /** returns the internal program ID*/
         virtual unsigned int internal_id() const = 0;
       protected:
         Program() {}
         virtual ~Program() {}
 
       private:
+
+        GLuint program_id_, shader_id_;
+
         Program(const Program&);
         Program& operator=(const Program&);
       };
 
 
-    };
+   }
 
-  }
 }
 
 
---
+++ iDK/src/program.cc
@@ -0,0 +1,147 @@
+#include "../include/dev/program.h"
+
+namespace iDK{
+
+  namespace dev{
+
+    // Program class constructor
+    // Creates a program in our program_id_
+    Program::Program(){
+      program_id_ = glCreateProgram();
+    }
+
+
+    // Attach the shader to our Program
+    void Program::attach(EDK3::dev::Shader *shader){
+
+      // FIX
+      // shader_id_ = shader->internal_id();
+      glAttachShader(program_id_, shader_id_);
+    }
+
+
+    // Links the current program
+    bool Program::link(EDK3::scoped_array<char> *link_log = 0L){
+      // Link the program
+      glLinkProgram(program_id_);
+
+      GLsizei program_lenght;
+      GLint program_size, program_linked;
+
+      glGetProgramiv(program_id_, GL_INFO_LOG_LENGTH, &program_size);
+      glGetProgramiv(program_id_, GL_LINK_STATUS, &program_linked);
+
+      // If link failed (Log can be generated withot any fail too)
+      // save the log and return it
+      if (link_log != 0L){
+        link_log->alloc(program_size);
+        glGetProgramInfoLog(program_id_, program_size, &program_lenght, link_log->get());
+      }
+
+      // Check if was succesfully linked
+      if (program_linked == GL_TRUE){
+        return true;
+      }else{
+        return false;
+      }
+
+    }
+
+
+    // Installs our program in current render state
+    void Program::use() const{
+      glUseProgram(program_id_);
+    }
+
+
+    // Returns the location of a attribute variable by name
+    int Program::get_attrib_location(const char *name) const{
+      return glGetAttribLocation(program_id_, name);
+    }
+
+
+    // Returns the location of a uniform position by name
+    int Program::get_uniform_position(const char *name) const{
+      return glGetUniformLocation(program_id_, name);
+    }
+
+
+    // Sets the uniform value depending on value type
+    void Program::set_uniform_value(const int uniform_pos, const EDK3::Type uniform_type, const float *value) const{
+      switch (uniform_type){
+        case EDK3::Type::T_FLOAT_1:
+          glUniform1fv(uniform_pos, 1, value);
+          break;
+        case EDK3::Type::T_FLOAT_2:
+          glUniform2fv(uniform_pos, 1, value);
+          break;
+        case EDK3::Type::T_FLOAT_3:
+          glUniform3fv(uniform_pos, 1, value);
+          break;
+        case EDK3::Type::T_FLOAT_4:
+          glUniform4fv(uniform_pos, 1, value);
+          break;
+        case EDK3::Type::T_MAT_2x2:
+          glUniformMatrix2fv(uniform_pos, 1, false, value);
+          break;
+        case EDK3::Type::T_MAT_3x3:
+          glUniformMatrix3fv(uniform_pos, 1, false, value);
+          break;
+        case EDK3::Type::T_MAT_4x4:
+          glUniformMatrix4fv(uniform_pos, 1, false, value);
+          break;
+        default:
+          printf(" *** This shouldnt be called *** ");
+          break;
+      }
+    }
+
+
+    // Sets the uniform value depending on value type
+    void Program::set_uniform_value(const int uniform_pos, const EDK3::Type uniform_type,const int *value) const{
+      switch (uniform_type){
+      case EDK3::Type::T_INT_1:
+        glUniform1iv(uniform_pos, 1, value);
+        break;
+      case EDK3::Type::T_INT_2:
+        glUniform2iv(uniform_pos, 1, value);
+        break;
+      case EDK3::Type::T_INT_3:
+        glUniform3iv(uniform_pos, 1, value);
+        break;
+      case EDK3::Type::T_INT_4:
+        glUniform4iv(uniform_pos, 1, value);
+        break;
+      default:
+        printf(" *** This shouldnt be called *** ");
+        break;
+      }
+    }
+
+
+    // Sets the uniform value depending on value type
+    void Program::set_uniform_value(const int uniform_pos, const EDK3::Type uniform_type, const unsigned int *value) const{
+      switch (uniform_type){
+      case EDK3::Type::T_UINT_1:
+        glUniform1uiv(uniform_pos, 1, value);
+        break;
+      case EDK3::Type::T_UINT_2:
+        glUniform2uiv(uniform_pos, 1, value);
+        break;
+      case EDK3::Type::T_UINT_3:
+        glUniform3uiv(uniform_pos, 1, value);
+        break;
+      case EDK3::Type::T_UINT_4:
+        glUniform4uiv(uniform_pos, 1, value);
+        break;
+      }
+    }
+
+    // Returns the program ID
+    unsigned int Program::internal_id() const{
+      return program_id_;
+    }
+
+  } //_ End dev namespace
+
+} //_ End iDK namespace
